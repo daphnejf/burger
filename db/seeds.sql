54cc4ae9de927716586b30990a2100655e21f1be
Daphne Jiménez <daphnejimenezf@gmail.com>
2019-12-12 19:41:25 -0600

    Added files

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers (id, burger_name, devoured, date)
+VALUES
+(null, "Beyond Famous Star", FALSE, null),
+(null, "Famous Star with Cheese", FALSE, null),
+(null, "Western Bacon Cheeseburger", FALSE, null),
+(null, "Beyond BBQ Cheeseburger", FALSE, null);
